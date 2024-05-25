42b42efab24240020b41f2d67efe8dc527835d0d
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-05-25 16:16:00 +0300

    added styles on my profile pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ import { TypeAnimation } from 'react-type-animation'
 
 const Hero = () => {
   return (
-    <div className='grid md:grid-cols-2 place-items-center max-w-[1250px] mx-auto bg-white mb-8 md:mb-32 mt-14'>
+    <div className='grid md:grid-cols-2 place-items-center max-w-[1250px] mx-auto mb-8 md:mb-32 mt-14'>
           <div className='max-w-[800px] '>
             <p className='text-gray-200 md:text-6xl text-3xl tracking-tight'>
                HEY, I AM <br />
-               <span>Dale Andrew</span> <br />
+               <span className='text-blue-200'>Dale Andrew</span> <br />
                <TypeAnimation 
                   sequence={[
                     "Developer", 1000,
@@ -22,11 +22,19 @@ const Hero = () => {
                />
             </p>
 
-            <h2></h2>
+            <h2 className='text-gray-300'>with 5x years of experience</h2>
+
+
+            <div className='flex flex-row gap-4 mb-4 md:mb-0'>
+              <button className='transform transition-transform hover:scale-105 hover:shadow-lg z-10 cursor-pointer font-bold text-gray-200 w-1/2 mt-6 p-2
+            fbg-gradient-to-r from-[#50a7c7] via-[#1f7c8f] to-[#3d6ca4] rounded-xl'>Download CV</button>
+
+              <button className='transform transition-transform hover:scale-105 hover:shadow-lg z-10 cursor-pointer font-bold text-gray-200 w-1/2 mt-6 p-2 border border-gray-400 rounded-xl'>View Work</button>
+            </div>
 
           </div>
   
-          <img src="" alt="" />
+          <img src={profilepic} alt="w-[300px] md:w-[500px]" />
         
     </div>
   )
